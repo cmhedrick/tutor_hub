92a2504475e4c56e9ab2336bf356f6f0fa9b1511
Chris Hedrick <christopher.m.hedrick@gmail.com>
2013-08-14 09:07:26 -0400

    updated models to work with sessions, emailauth, and expertise

--- tutorhub/admin.py
+++ tutorhub/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from models import Tutor, Student
+from models import Tutor, Student, Session, Expertise, Availability, EmailAuth
 
 admin.site.register(Tutor)
 admin.site.register(Student)
+admin.site.register(Session)
+admin.site.register(Expertise)
+admin.site.register(Availability)
+admin.site.register(EmailAuth)
--- tutorhub/models.py
+++ tutorhub/models.py
@@ -3,22 +3,44 @@ from django.db import models
 # Create your models here.
 
 class Tutor(models.Model):
-    years = models.IntegerField()
-    availability = models.DateField()
-    name = models.CharField(max_length=35)
+    tutor_email = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    phone = models.CharField(max_length=15)
     picture = models.ImageField(upload_to='images/%Y/%m/%d')
-    email = models.CharField(max_length=100)
-    room = models.CharField(max_length=4)
-    subject = models.CharField(max_length=35)
-    phone = models.CharField(max_length=14)
 
 class Student(models.Model):
-    first_name = models.CharField(max_length=35)
-    last_name = models.CharField(max_length=35)
+    student_id = models.CharField(max_length=35)
+    email = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    phone = models.CharField(max_length=15)
+    grade = models.CharField(max_length=2)
     visited = models.BooleanField()
     email = models.CharField(max_length=100)
     regular_teacher = models.CharField(max_length=35)
-    session = models.CharField(max_length=35)
-    grade = models.IntegerField()
+
+class Session(models.Model):
     student_id = models.CharField(max_length=35)
-    assignment = models.CharField(max_length=250)
+    date = models.DateField()
+    building = models.CharField(max_length=25)
+    room = models.CharField(max_length=4)
+    topic = models.CharField(max_length=25)
+    assignments = models.CharField(max_length=100)
+
+class Expertise(models.Model):
+    tutor_email = models.CharField(max_length=100)
+    subject = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = "Expertise"
+
+class Availability(models.Model):
+    tutor_email = models.CharField(max_length=100)
+    date = models.DateField()
+
+    class Meta:
+        verbose_name_plural = "Availabilities"
+
+class EmailAuth(models.Model):
+    tutor_email = models.CharField(max_length=100)
