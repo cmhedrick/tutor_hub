02a7fd5081f440cdab7bdde81ba77fb3275be146
Chris Hedrick <christopher.m.hedrick@gmail.com>
2013-08-15 08:29:24 -0400

    changed admin.py to import the email_authentication

--- tutorhub/admin.py
+++ tutorhub/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from models import Tutor, Student, Session, Expertise, Availability, EmailAuth
+from models import Tutor, Student, Session, Expertise, Availability, Email_Authentication
 
 admin.site.register(Tutor)
 admin.site.register(Student)
 admin.site.register(Session)
 admin.site.register(Expertise)
 admin.site.register(Availability)
-admin.site.register(EmailAuth)
+admin.site.register(Email_Authentication)
